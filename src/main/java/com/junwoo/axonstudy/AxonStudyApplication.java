0ab0639ebb102f1efa6bc38c6a51208d4061189a
junnukim1007gmail.com <junnukim1007@gmail.com>
2026-01-05 11:34:04 +0900

    project init

---
+++ src/main/java/com/junwoo/axonstudy/AxonStudyApplication.java
@@ -0,0 +1,13 @@
+package com.junwoo.axonstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junwoo/axonstudy/AxonStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junwoo.axonstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
